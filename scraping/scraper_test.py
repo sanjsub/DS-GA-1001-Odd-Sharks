ce30cd45ceb367bc3b18e53bda702a8dc154328d
Pranab Islam <islampranab@gmail.com>
2020-11-03 21:22:49 -0500

    updated small change

--- scraping/scraper_test.py
+++ scraping/scraper_test.py
@@ -12,7 +12,7 @@ all seasons and then repeat the scraping, write to CSV files and store
 
 url = 'https://www.betexplorer.com/basketball/usa/nba/results/?stage=bPyjUVy1&month=all'
 url_2018 = 'https://www.betexplorer.com/basketball/usa/nba-2018-2019/results/?stage=8Y7SorQp&month=all'
-
+url_2016 = 'https://www.betexplorer.com/basketball/usa/nba-2016-2017/results/?stage=zPq9t1nb&month=all'
 
 unique_identifier = 'table-main h-mb15 js-tablebanner-t js-tablebanner-ntb'
 
@@ -33,8 +33,8 @@ headers = {
 
 # %%
 
-#page = requests.get(url)
-page = requests.get(url_2018)
+
+page = requests.get(url_2016)
 soup = BeautifulSoup(page.text, "html.parser")
 table = soup.find("table", attrs={"class":"table-main h-mb15 js-tablebanner-t js-tablebanner-ntb"})
 rows = table.find_all("tr")
