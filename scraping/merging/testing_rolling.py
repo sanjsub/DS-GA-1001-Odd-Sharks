468e695ab9ab4b99c631b2fb5c7a6cab6886136f
Pranab Islam <islampranab@gmail.com>
2020-11-24 14:13:31 -0500

    running loop

--- scraping/merging/testing_rolling.py
+++ scraping/merging/testing_rolling.py
@@ -5,17 +5,19 @@ import sys
 sys.path.append("cleaned_dfs_11-23/")
 from rollstats_and_oddsfilter import roll_stats
 import pandas as pd
-years = list(range(2009, 2021))
+years = list(range(2019, 2020))
 #year = 2009
 
 #bookie_df = pd.read_csv('..\DS-GA-1001-Odd-Sharks\scraping\merging\cleaned_dfs_11-23\{}_stats.csv'.format(year), parse_dates=['DATE'])
 # %%
-lst = []
-for year in years:
-    bookie_df = pd.read_csv('cleaned_dfs_11-23/{}_stats.csv'.format(year), parse_dates=['DATE'])
-    # bookie_df = pd.read_csv('..\DS-GA-1001-Odd-Sharks\scraping\merging\cleaned_dfs_11-23\{}_stats.csv'.format(year), parse_dates=['DATE'])
-    lst.append(roll_stats(bookie_df, n=10))
-# %%
-for model, year in zip(lst, years):
-    model.to_csv("tester_" + str(year) + ".csv")
+for rolling_window in range(5, 6):    
+    all_seasons = []
+    for year in years:
+        bookie_df = pd.read_csv('cleaned_dfs_11-23/{}_stats.csv'.format(year), parse_dates=['DATE'])
+        # bookie_df = pd.read_csv('..\DS-GA-1001-Odd-Sharks\scraping\merging\cleaned_dfs_11-23\{}_stats.csv'.format(year), parse_dates=['DATE'])
+        all_seasons.append(roll_stats(bookie_df, n=rolling_window))
+
+    for model, year in zip(all_seasons, years):
+        output_path = 'all_rolling_windows/'+ str(year) + "_n"+ str(rolling_window) + ".csv"
+        model.to_csv(output_path, index=False)
 # %%
