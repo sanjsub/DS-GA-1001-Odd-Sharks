74ba0f161a4bab1835ad052940883589d0a62c4c
Pranab Islam <islampranab@gmail.com>
2020-11-24 01:09:43 -0500

    testing screen fx

--- scraping/merging/testing_rolling.py
+++ scraping/merging/testing_rolling.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append("cleaned_dfs_11-23/")
 from rollstats_and_oddsfilter import roll_stats
 import pandas as pd
-years = list(range(2009, 2010))
+years = list(range(2009, 2021))
 #year = 2009
 
 #bookie_df = pd.read_csv('..\DS-GA-1001-Odd-Sharks\scraping\merging\cleaned_dfs_11-23\{}_stats.csv'.format(year), parse_dates=['DATE'])
@@ -16,5 +16,6 @@ for year in years:
     # bookie_df = pd.read_csv('..\DS-GA-1001-Odd-Sharks\scraping\merging\cleaned_dfs_11-23\{}_stats.csv'.format(year), parse_dates=['DATE'])
     lst.append(roll_stats(bookie_df, n=10))
 # %%
-lst[0].to_csv("tester.csv")
+for model, year in zip(lst, years):
+    model.to_csv("tester_" + str(year) + ".csv")
 # %%
