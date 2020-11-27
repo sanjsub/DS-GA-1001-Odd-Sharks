fa1d61c1951e7bfbb8306fbdc93b434fbfa10a25
Pranab Islam <islampranab@gmail.com>
2020-11-27 18:06:35 -0500

    updated construct df function

--- model_feature_selection/basic_feature_analysis_pi.py
+++ model_feature_selection/basic_feature_analysis_pi.py
@@ -8,12 +8,47 @@ import os
 
 label = 'Underdog Win'
 label_ind = 10 ## After you drop the columns
-cols_drop = ['unique_game_id', 'DATE', 'Home Team', 'Away Team']
+std_cols_drop = ['unique_game_id', 'DATE', 'Home Team', 'Away Team']
+additional_drop = ['Home Team Cut_offensive',
+       'Away Team Cut_offensive', 'Home Team Handoff_defensive',
+       'Away Team Handoff_defensive', 'Home Team Handoff_offensive',
+       'Away Team Handoff_offensive', 'Home Team Isolation_defensive',
+       'Away Team Isolation_defensive', 'Home Team Isolation_offensive',
+       'Away Team Isolation_offensive', 'Home Team Misc_offensive',
+       'Away Team Misc_offensive', 'Home Team OffScreen_defensive',
+       'Away Team OffScreen_defensive', 'Home Team OffScreen_offensive',
+       'Away Team OffScreen_offensive', 'Home Team PnRBH_defensive',
+       'Away Team PnRBH_defensive', 'Home Team PnRBH_offensive',
+       'Away Team PnRBH_offensive', 'Home Team PnRRM_defensive',
+       'Away Team PnRRM_defensive', 'Home Team PnRRM_offensive',
+       'Away Team PnRRM_offensive', 'Home Team Postup_defensive',
+       'Away Team Postup_defensive', 'Home Team Postup_offensive',
+       'Away Team Postup_offensive', 'Home Team Putbacks_defensive',
+       'Away Team Putbacks_defensive', 'Home Team Putbacks_offensive',
+       'Away Team Putbacks_offensive', 'Home Team Spotup_defensive',
+       'Away Team Spotup_defensive', 'Home Team Spotup_offensive',
+       'Away Team Spotup_offensive', 'Home Team Transition_defensive',
+       'Away Team Transition_defensive', 'Home Team Transition_offensive',
+       'Away Team Transition_offensive']
+
+non_relative_columns = ['AWAY_CUM_WIN_PCT', 'AWAY_IP_AVAIL','AWAY_PRIOR_WIN_V_OPP',
+                        'AWAY_ROLL_WIN_PCT', 'Away 3P', 'Away 3P%', 'Away 3PAr', 'Away AST',
+                        'Away AST%', 'Away All Stars', 'Away BLK', 'Away BLK%', 'Away DRB',
+                        'Away DRB%', 'Away DRtg', 'Away FG', 'Away FG%', 'Away FT', 'Away FT%',
+                        'Away FTr', 'Away ORB', 'Away ORB%', 'Away ORtg', 'Away Odds Close',
+                        'Away PF', 'Away STL', 'Away STL%', 'Away TOV', 'Away TOV%', 'Away TS%',
+                        'Away eFG%', 'HOME_CUM_WIN_PCT', 'HOME_IP_AVAIL', 'HOME_PRIOR_WIN_V_OPP',
+                        'HOME_ROLL_WIN_PCT', 'Home 3P', 'Home 3P%', 'Home 3PAr', 'Home AST',
+                        'Home AST%', 'Home All Stars', 'Home BLK', 'Home BLK%', 'Home DRB',
+                        'Home DRB%', 'Home DRtg', 'Home FG', 'Home FG%', 'Home FT', 'Home FT%',
+                        'Home FTr', 'Home ORB', 'Home ORB%', 'Home ORtg', 'Home Odds Close',
+                        'Home PF', 'Home STL', 'Home STL%', 'Home TOV', 'Home TOV%', 'Home TS%',
+                        'Home eFG%']
 
 main_path = '..\scraping\merging\cleaned_dfs_11-23\\all_rolling_windows\\'
 filename = '\\2015_stats_n10.csv'
 
-def construct_df(years, n):
+def construct_df(years, n, main_path, drop_playstyle=True):
     '''
     Given that our files are located in "..\scraping\merging\cleaned_dfs_11-23\\all_rolling_windows",
     return concatenated df for a given rolling avg window (n) and a list of years
@@ -21,13 +56,31 @@ def construct_df(years, n):
     years: type list of ints from 2009 - 2020
     n: type in from 5 to 50 inclusive
     '''
-    ## Maybe special case for dfs including 2013- and 2014+ need to be included
+    
     filenames = [str(year) + "_stats_n" + str(n) + ".csv" for year in years]
     dfs = []
+
     for filename in filenames:
         dfs.append(pd.read_csv(main_path + filename))
+    if drop_playstyle:
+        cols_drop = std_cols_drop + additional_drop
+    ## Currently assuming that we are dropping playstyle stuff so wont create robust working for it
+    ## remember to drop 'Away Underdog' after all of this is done
 
     concatenated_df = pd.concat(dfs).drop(columns=cols_drop)
+    concatenated_df['Away Underdog'].replace(0, -1, inplace=True)
+
+    new_column_tuples = []
+    for i in range(int(len(non_relative_columns)/2)):
+        new_column_tuples.append((non_relative_columns[i],non_relative_columns[i+31]))
+
+    for col_pair in new_column_tuples:
+        concatenated_df['underdog_rel_' + col_pair[0][5:]] = \
+            (concatenated_df[col_pair[0]] - concatenated_df[col_pair[1]])*concatenated_df['Away Underdog']
+
+    concatenated_df['Home Relative Age Diff'] = concatenated_df['Home Relative Age Diff']*concatenated_df['Away Underdog']*1 
+    concatenated_df.drop(columns=non_relative_columns, inplace=True)
+
     return concatenated_df
 
 
