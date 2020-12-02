db4cfb3e8b9f38bd7340bebd73d993c8f3560ea5
Jack Jones <jackjones@Jacks-MacBook-Pro.local>
2020-12-01 21:54:36 -0500

    svm cv and comparison

--- model_feature_selection/svm_analysis/svm_comparison.py
+++ model_feature_selection/svm_analysis/svm_comparison.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import classification_report
 from joblib import load
-
+from model_feature_selection.basic_feature_analysis_pi import construct_df
+import matplotlib.pyplot as plt
 
 dump_dir = "./svm_models/"
 dump_prefix = "svm_cv_n"
@@ -35,5 +42,61 @@ for i in range(5, 51):
 	cv_results[i]["param_kernel"] = "linear"
 	cv_results[i] = pd.concat([cv_results[i], cv_results_rbf_no_smote, cv_results_rbf_smote])
 	cv_results[i]["roll_num"] = i
-cv_results_merged = pd.concat(cv_results.values())
-print(cv_results_merged)
+df = pd.concat(cv_results.values())
+
+df["balancing"] = df["param_class_weight"]
+df["balancing"][df["balancing"] == "NA"] = "smote"
+df["balancing"][df["balancing"].isnull()] = "none"
+df["balancing"][df["balancing"] == "balanced"] = "class_weight"
+df.reset_index(drop=True, inplace=True)
+grouped_df = df.groupby(["param_kernel", "balancing", "param_C"])
+max_f1 = grouped_df.max()["mean_test_score"].reset_index()
+idxmax_f1 = list(grouped_df.idxmax()["mean_test_score"])
+max_f1["roll_num"] = list(df.iloc[idxmax_f1,]["roll_num"])
+
+plt.figure(figsize=(20,10))
+for kernel in ["linear", "rbf"]:
+    for balancing in ["class_weight", "none", "smote"]:
+        data = max_f1[(max_f1["param_kernel"] == kernel) & (max_f1["balancing"] == balancing)]
+        label = ("kernel: " + kernel + ", balancing method: " + balancing)
+        x_vals = np.log10(data["param_C"]).astype(int)
+        y_vals = data["mean_test_score"]
+        plt.plot(x_vals, y_vals, label=label)
+        for x_val, y_val, roll_num in zip(x_vals, y_vals, data["roll_num"]):
+            plt.annotate(str(roll_num), (x_val, y_val + 0.01))
+plt.xticks(x_vals)
+plt.legend()
+plt.xlabel("Log10 of Regularization Parameter C")
+plt.ylabel("Maximum of Mean F1 Score")
+plt.title("Maximum of Mean 5-Fold F1 Score over all Rolling Averages")
+plt.savefig("../../images/SVM_f1_Comparisons.png")
+
+opt_model_idx = max_f1.groupby(["param_kernel", "balancing"])["mean_test_score"].idxmax()
+opt_models = max_f1.iloc[opt_model_idx,]
+main_path = "../../scraping/merging/cleaned_dfs_11-23/all_rolling_windows/"
+dump_prefix = "svm_final_n"
+def create_svm_model(kernel, balancing, C, num_roll):
+    train = construct_df(range(2009, 2019), n=num_roll, main_path=main_path)
+    test = construct_df([2019, 2020], n=num_roll, main_path=main_path)
+    x_train, x_test = train.drop(columns="Underdog Win"), test.drop(columns="Underdog Win")
+    y_train, y_test = train["Underdog Win"], test["Underdog Win"]
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train_scaled = scaler.transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+    balance_param = None
+    if balancing == "class_weight":
+        balance_param = "balanced"
+    elif balancing == "smote":
+        smote = SMOTE(random_state=18)
+        x_train_scaled, y_train_scaled = smote.fit_resample(x_train_scaled, y_train)
+    if kernel == "rbf":
+        clf = SVC(random_state=18, kernel=kernel, class_weight=balance_param, C=C)
+    else:
+        clf = LinearSVC(random_state=18, class_weight=balance_param, C=C)
+    clf.fit(x_train, y_train)
+    preds = clf.predict(x_test)
+    return classification_report(y_test, preds)
+for i, row in opt_models.iterrows():
+    print(row["param_kernel"], row["balancing"], row["param_C"])
+    print(create_svm_model(row["param_kernel"], row["balancing"], row["param_C"], row["roll_num"]))
--- model_feature_selection/svm_analysis/svm_cv.py
+++ model_feature_selection/svm_analysis/svm_cv.py
@@ -4,7 +4,6 @@ from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC, LinearSVC
-from sklearn.metrics import classification_report
 from joblib import dump
 from model_feature_selection.basic_feature_analysis_pi import construct_df
 
@@ -15,27 +14,27 @@ dump_prefix = "svm_cv_n"
 C = [1, 10, 100, 1000]
 for i in range(5, 51):
 	train = construct_df(range(2009, 2019), n=i, main_path=main_path)
-	test = construct_df([2019, 2020], n=i, main_path=main_path)
-	x_train, x_test = train.drop(columns="Underdog Win"), test.drop(columns="Underdog Win")
-	y_train, y_test = train["Underdog Win"], test["Underdog Win"]
+	x_train = train.drop(columns="Underdog Win")
+	y_train= train["Underdog Win"]
 	scaler = StandardScaler()
 	scaler.fit(x_train)
 	x_train_scaled = scaler.transform(x_train)
-	x_test_scaled = scaler.transform(x_test)
+	smote = SMOTE(random_state=18)
+	x_res, y_res = smote.fit_resample(x_train_scaled, y_train)
+
+	# linear unbalanced and weighted data
 	tuned_params = [{"C": C, "class_weight": [None, "balanced"]}]
 	clf = GridSearchCV(LinearSVC(random_state=18), tuned_params, scoring="f1")
 	clf.fit(x_train_scaled, y_train)
 	dump(clf, dump_dir + dump_prefix + str(i) + "_linear.joblib")
 
-	# Balance Data
-	smote = SMOTE(random_state=18)
-	x_res, y_res = smote.fit_resample(x_train_scaled, y_train)
+	# linear smote data
 	tuned_params = [{"C": C}]
 	clf = GridSearchCV(LinearSVC(random_state=18), tuned_params, scoring="f1")
 	clf.fit(x_res, y_res)
 	dump(clf, dump_dir + dump_prefix + str(i) + "_linear_smote.joblib")
 
-	# rbf
+	# rbf unbalanced and weighted data
 	tuned_params = {"kernel": ["rbf"], "C": C, "class_weight": [None, "balanced"]}
 	clf = GridSearchCV(SVC(random_state=18), tuned_params, scoring="f1")
 	clf.fit(x_train_scaled, y_train)
